3048033f53c169581612c30c85372c3ff2ac7bce
flemzy-netizen <zerosex4545@gmail.com>
2026-03-20 11:00:16 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Google Sheets Script URL - PASTE YOUR ACTUAL URL HERE
-const SCRIPT_URL = 'https://script.google.com/macros/s/YOUR_ACTUAL_URL/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzoE2Vgo-OUzmCSFwunB_LQH8uT-ICBXs3wDNHcrz5ufschuOplLDK50V3W7nzCaNmCcA/exec';
 
 // Admin credentials
 const ADMIN_USERNAME = 'flemzy@admin.com';
