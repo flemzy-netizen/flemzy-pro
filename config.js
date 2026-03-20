3a506e5ac5860e748a9091c22047d87b9fa58e70
flemzy-netizen <zerosex4545@gmail.com>
2026-03-20 02:38:44 -0700

    Update config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    SCRIPT_URL:'https://script.google.com/macros/s/AKfycbxUJh4aRdwQ2pWnLzwbXRW690kSF4kMimgw8Z_hEtO6pDUlt_VWqarS5Iu3cHizpo6XCQ/exec' 
+    ADMIN: {
+        email: 'flemzy@admin.com',
+        password: 'Admin@123'
+    }
+};
